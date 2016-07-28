0ccdd686e4037de8e0201612d376f7cfb8b2b67b
IanZheng <flks_2005@qq.com>
2016-07-28 22:42:58 +0800

    update

--- Test/app/src/main/java/com/ian/test/MainActivity.java
+++ Test/app/src/main/java/com/ian/test/MainActivity.java
@@ -3,6 +3,8 @@ package com.ian.test;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,7 +12,24 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.array_adapter_test);
+
+        ListView list1 = (ListView) findViewById(R.id.list1);
+
+        String[] arr1 = {"孙悟空","猪八戒","沙和尚"};
+
+        ArrayAdapter<String> adapter1 = new ArrayAdapter<String>(this, R.layout.array_item
+            , arr1);
+
+        list1.setAdapter(adapter1);
+
+        ListView list2 = (ListView)findViewById(R.id.list2);
+
+        String[] arr2 = {"Java", "Hibernate", "Spring", "Android"};
+
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(this,R.layout.array_item
+            ,arr2);
+        list2.setAdapter(adapter2);
     }
 
     public void clickHandler(View source){
